c5df7c5089d26c26195f2a50ba7ef0e08f13faf9
itsjustrobert <robertlove99999@gmail.com>
2021-07-21 18:33:45 -0500

    completed lab 03

---
+++ app.js
@@ -0,0 +1,36 @@
+// do a sanity test
+// console.log("sanity test passed");
+
+
+
+// data types
+// string
+let myString = 'my string';
+// numbers
+let myNumber= 10;
+let myNumber2 = 3.14;
+let myNumber3 ="3.14";
+// boolean true or false
+let isTrue = true;
+let isFalse = false;
+let age = 23;
+let age2= 24;
+let value = 2112
+
+// expressions
+let result =((myNumber2===myNumber3||age===myNumber3)|| value === 2112);
+console.log(result);
+
+if(value===2112){
+    console.log("that's a great album");
+    
+}
+else if(age===23){
+    console.log("ok then i see youre 23");
+}
+
+// classic for loop
+for (let index = 0; index <100; index++) {
+    console.log("still loopin " + index);
+    
+}
--- lab2/app.js
+++ lab2/app.js
@@ -1,100 +1,123 @@
 // function greeting the user
-function greetings(){
-    let greeting=alert("Hi there, my name is Robert, let's get to know each other");
-console.log(greeting);
+function greetings() {
+  let greeting = alert(
+    "Hi there, my name is Robert, let's get to know each other"
+  );
+  console.log(greeting);
 
-// getting the user's name
-userName=prompt(
-    "What's your name, I would love to know it?"
-)
-console.log(userName);
-// greeting the user}
-document.write(userName);
+  // getting the user's name
+  userName = prompt("What's your name, I would love to know it?");
+  console.log(userName);
+  // greeting the user}
+  document.write(userName);
 
-alert("So nice to finally met you, " + userName +"!");
-
-
-
+  alert("So nice to finally met you, " + userName + "!");
 }
+
 // function gauging the user's interests
-function interests(){
-
-alert("lets play a yes or no game " + userName + "! To get to know each other better.");
-let sports=prompt("Do you like sports "+ userName +"?");
-console.log(sports)
-if(sports==="yes"){
-   favoriteSport= prompt("which is your favorite?");
-   console.log(favoriteSport);
-}
-
-else{
+function interests() {
+  alert(
+    "lets play a yes or no game " +
+      userName +
+      "! To get to know each other better."
+  );
+  let sports = prompt("Do you like sports " + userName + "?");
+  console.log(sports);
+  if (sports === "yes") {
+    favoriteSport = prompt("which is your favorite?");
+    console.log(favoriteSport);
+  } else {
     prompt("why not?");
-    
-}
-let food=prompt("Do you eat steak "+ userName +"?");
-console.log(sports);
-if(food==="yes"){
-   favoriteSteak= prompt("How do you take your steak "+ userName + "?");
-   console.log(favoriteSteak);
-}
-
-else{
-   let preference= prompt("What do you prefer?");
-   console.log(preference);
-   document.write(preference);
-   alert("I absolutely love " + preference)
-}
- let adidas=prompt(userName +" my favorite brand is Adidas, dont you just love them?");
- console.log(adidas);
- if(adidas==="yes"){
-    adidasPreference= prompt("Do you prefer the shoes or the clothing?");
+  }
+  let food = prompt("Do you eat steak " + userName + "?");
+  console.log(food);
+  if (food === "yes") {
+    favoriteSteak = prompt("How do you take your steak " + userName + "?");
+    console.log(favoriteSteak);
+  } else {
+    let preference = prompt("What do you prefer?");
+    console.log(preference);
+    document.write(preference);
+    alert("I absolutely love " + preference);
+  }
+  let adidas = prompt(
+    userName + " my favorite brand is Adidas, dont you just love them?"
+  );
+  console.log(adidas);
+  if (adidas === "yes") {
+    adidasPreference = prompt("Do you prefer the shoes or the clothing?");
     console.log(adidasPreference);
- }
-
- else{
-   let brand= prompt("Oooh what brand do you like then?");
-    alert("I cant lie, I like "  + brand +" too, lowkey.");
+  } else {
+    let brand = prompt("Oooh what brand do you like then?");
+    alert("I cant lie, I like " + brand + " too, lowkey.");
     console.log(brand);
-
- }
- let videoGames=prompt("Do you like sports "+ userName +"?");
- console.log(videoGames);
- if(videoGames==="yes"){
-   favoriteGame= prompt("which is your favorite game?");
+  }
+  let videoGames = prompt("Do you like video games " + userName + "?");
+  console.log(videoGames);
+  if (videoGames === "yes") {
+    favoriteGame = prompt("which is your favorite game?");
     console.log(favoriteGame);
-   
- }
- else{
-     let freeTime=prompt("why not? And what do you like to do in your free time?");
-     console.log(freeTime);
-     alert(freeTime +" is great fun, " + userName);
-     document.write(freeTime);
- }
- let coding=prompt("Are you a coder "+ userName +"?");
- console.log(coding);
- if(coding==="yes"){
-   goal= prompt("What do you wish to achieve in this endeavor");
-   console.log(goal);
- }
+  } else {
+    let freeTime = prompt(
+      "why not? And what do you like to do in your free time?"
+    );
+    console.log(freeTime);
+    alert(freeTime + " is great fun, " + userName);
+    document.write(freeTime);
+  }
+  let coding = prompt("Are you a coder " + userName + "?");
+  console.log(coding);
+  if (coding === "yes") {
+    goal = prompt("What do you wish to achieve in this endeavor");
+    console.log(goal);
+  } else {
+    let = job = prompt("What do you do for a living?");
+    console.log(job);
+    document.write(job);
+  }
+  alert(
+    "It has been a pleasure meeting you " +
+      userName +
+      "! I hope with this webpage you will know more about me."
+  );
 
- else{
-  let=job= prompt("What do you do for a living?");
-   console.log(job);
-   document.write(job);
+  confirm("Enter?");
 }
-alert("It has been a pleasure meeting you " +userName + "! I hope with this webpage you will know more about me.");
-confirm("Enter?");
+function guessMe(){
 
+
+
+  let correctNumber = 9;
+
+  alert(
+    "I'm thinking of a number...Can you guess it? It's between 1 and 15...."
+  );
+
+  // let numberGuessed=[
+  // prompt("please enter your first guess"),
+  // prompt("please enter your second guess"),
+  // prompt("please enter your third guess"),
+  // prompt("please enter your last guess"),
+  // ];
+
+  // console.log(numberGuessed);
+  let wrongNumberGuessed = true;
+
+  while (wrongNumberGuessed) {
+    numberGuessed = prompt("Guess a number");
+    if (numberGuessed == correctNumber) {
+       wrongNumberGuessed=false;
+      alert("you are correct!");
+    } else if (numberGuessed < correctNumber) {
+      alert("your number is far too low...try again");
+    } else {
+      alert("your number is too high");
+    }
+
+    // console.log([i]);
+  }
 }
+
 greetings();
 interests();
-
-
-
-
-
-
-
-
-
-  
+guessMe();
